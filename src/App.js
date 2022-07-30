afc86858b8ebb5978f6c01862a46e1a3d339586e
rahulmaurya-exe <rahulmaurya0001@gmail.com>
2022-07-30 23:38:55 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
